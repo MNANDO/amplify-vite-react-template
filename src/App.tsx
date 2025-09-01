2f1fd34558b93f026f0b6d629bce7756402a775e
Michael Hernando <mhernando334@gmail.com>
2025-09-01 17:30:26 -0400

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 const client = generateClient<Schema>();
 
 function App() {
     const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-    const { signOut } = useAuthenticator();
+    const { user, signOut } = useAuthenticator();
 
     useEffect(() => {
         client.models.Todo.observeQuery().subscribe({
@@ -21,21 +21,21 @@ function App() {
 
     return (
         <main>
-        <h1>My todos</h1>
-        <button onClick={createTodo}>+ new</button>
-        <ul>
-        {todos.map((todo) => (
-            <li key={todo.id}>{todo.content}</li>
-        ))}
-        </ul>
-        <div>
-        🥳 App successfully hosted. Try creating a new todo.
-            <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-        Review next step of this tutorial.
-            </a>
-        </div>
-        <button onClick={signOut}>Sign out</button>
+            <h1>{user?.signInDetails?.loginId}'s todos</h1>
+            <button onClick={createTodo}>+ new</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>{todo.content}</li>
+                ))}
+            </ul>
+            <div>
+                🥳 App successfully hosted. Try creating a new todo.
+                <br />
+                <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+                    Review next step of this tutorial.
+                </a>
+            </div>
+            <button onClick={signOut}>Sign out</button>
         </main>
     );
 }
